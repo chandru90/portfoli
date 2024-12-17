3d1eed58070103a531e0b9c18a15376ce4cbc690
chandru90 <iochandru90@gmail.com>
2024-12-17 08:04:28 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Hero from "./components/Hero";
-import Internships from "./components/internship";
+import Internships from "./components/Internships";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
